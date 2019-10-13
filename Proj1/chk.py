4e381b8a6dde324063647da9aaf8609516519285
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2019-10-13 20:57:12 +0100

    Proj1 begin.

---
+++ Proj1/chk.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# File:  chk.py
+# Author:  mikolas
+# Created on:  Fri Oct 11 14:18:32 WEST 2019
+# Copyright (C) 2019, Mikolas Janota
+import sys
+def err(msg):
+    print("ERROR:", msg)
+    exit(1)
+
+def parse_samples(f):
+    nms = None
+    samples = []
+    for l in f:
+        s = l.rstrip().split()
+        if not s: continue
+        if nms:
+            samples.append([int(l) for l in s])
+        else:
+            nms = [int(l) for l in s]
+    return (nms, samples)
+
+def chk(ls, samples):
+    lns = dict() #  left children
+    rns = dict() #  right children
+    a = dict() #  assigned features
+    tl = set() #  leaf true
+    fl = set() #  leaf  false
+    for l in ls:
+        l = l.rstrip()
+        if not l: continue
+        if l == 'UNSAT':
+            print('OK (UNSAT)')
+            return
+        spl = l.split()
+        if spl[0] == 'l' or spl[0] == 'r':
+            vs = [ int(s) for s in spl[1:] ]
+            assert(len(vs)==2)
+            if spl[0] == 'l':
+                if vs[0] in lns: err("{}  already has left child".format(vs[0]))
+                lns[vs[0]] = vs[1]
+            else:
+                if vs[0] in rns: err("{}  already has right child".format(vs[0]))
+                rns[vs[0]] = vs[1]
+        if spl[0] == 'c':
+            vs = [ int(s) for s in spl[1:] ]
+            assert(len(vs)==2)
+            if vs[1] == 0:
+                fl.add(vs[0])
+            elif vs[1] == 1:
+                tl.add(vs[0])
+            else:
+                assert(False)
+        if spl[0] == 'a':
+            vs = [ int(s) for s in spl[1:] ]
+            assert(len(vs)==2)
+            if vs[1] in a: err("{}  already has assigned feature".format(vs[1]))
+            a[vs[1]] = vs[0]
+
+    def check_structure(nd, visited):
+        if nd in visited: err("there is a cycle on node {}".format(nd))
+        visited.add(nd)
+        hl = nd in lns
+        hr = nd in rns
+        if hl != hr: err("{} can only have zero or two children".format(nd))
+        if not hl:
+            if nd in a: err("{} is a leaf and therefore it  cannot have have a feature assigned".format(nd))
+            if nd not in fl and nd not in tl: err("{} is a leaf and therefore it has to have a class  assigned".format(nd))
+            return
+        if nd not in a: err("{} is internal and therefore it has to have a  feature assigned".format(nd))
+        if nd in fl or nd in tl: err("{} is internal and therefore it  cannot have have a  class assigned".format(nd))
+        check_structure(lns[nd], visited)
+        check_structure(rns[nd], visited)
+
+    check_structure(1, set())
+
+    def get_val(nd, sample):
+        if nd in fl: return 0
+        if nd in tl: return 1
+        ftr = a[nd]
+        nxt = lns[nd] if sample[ftr - 1] == 0 else rns[nd] 
+        return get_val(nxt, sample)
+
+    for sample  in samples:
+        if sample[-1] != get_val(1, sample):
+            err('FAIL on sample {} '.format(sample))
+            return False
+        else:
+            print('OK on {} '.format(sample))
+    return True
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('USAGE: {} <sample-file>'.format(sys.argv[0]))
+        exit(1)
+
+    with open(sys.argv[1]) as sf:
+        nms, samples = parse_samples(sf)
+
+    if chk(sys.stdin, samples):
+        print('OK')
+
---
+++ Proj1/stub.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+# File:  stub.py
+# Author:  mikolas
+# Created on:  Sat Oct 12 10:30:54 WEST 2019
+# Copyright (C) 2019, Mikolas Janota
+import sys,subprocess 
+
+solver = './lingeling'
+
+def neg(l): return l[1:] if l[0] == '-' else '-'+l
+def var(l): return l[1:] if l[0] == '-' else l
+def sign(l): return l[0] == '-'
+
+class Enc:
+    def __init__(self, input_count,  node_count):
+         self.node_count = node_count
+         self.input_count = input_count
+         self.constraints = []
+         self.fresh = 0
+
+    def x(self,i): return 'x_{}'.format(i)
+    def y(self,i): return 'y_{}'.format(i)
+
+    def add_constraint(self, constraint):
+        '''add constraints, which is a list of literals'''
+        self.constraints.append(constraint)
+
+    def mk_fresh(self, nm):
+        '''make fresh variable'''
+        self.fresh = self.fresh + 1
+        return '_' + nm + '__' + str(self.fresh)
+
+    def mk_and(self, l1, l2):
+        '''encode and between l1 and l2 by introducing a fresh variable'''
+        r = self.mk_fresh(l1+'_and_'+l2)
+        self.constraints.append([neg(l1), neg(l2), r])
+        self.constraints.append([l1, neg(r)])
+        self.constraints.append([l2, neg(r)])
+        return r
+
+    def add_iff(self, l1, l2):
+        '''add iff constraint between l1 and l2'''
+        self.constraints.append([neg(l1), l2])
+        self.constraints.append([l1, neg(l2)])
+
+
+    def print_model(self,model):
+        '''prints SAT model, eventually should print the decision tree'''
+        print('# === model')
+        for str_var in sorted(self.var_map.keys()):
+            v = self.var_map[str_var]
+            val = '?'
+            if v in model and model[v]: val='T'
+            if v in model and not model[v]: val='F'
+            print('# {}={} ({})'.format(str_var,val,v))
+        print('# === end of model')
+        print('# === tree (TODO)')
+        print('# === end of tree')
+
+
+    def mk_cnf(self,print_comments):
+        '''encode constraints as CNF in DIMACS'''
+        maxid = 0
+        self.var_map = dict()
+        cs = 0
+        rv = ''
+        for c in self.constraints:
+            if not isinstance(c, list): continue
+            cs = cs + 1
+            for l in c:
+                if var(l) not in self.var_map:
+                    maxid = maxid + 1
+                    self.var_map[var(l)] = maxid
+
+        rv += 'p cnf {} {}'.format(len(self.var_map), cs) + '\n'
+        for c in self.constraints:
+            if isinstance(c, list):
+                if print_comments:
+                    rv += 'c ' + str(c) + '\n'
+                rv += ' '.join(map(str,[ -(self.var_map[var(l)]) if sign(l) else self.var_map[l] for l in c])) + ' 0\n'
+            else:
+                if print_comments:
+                    rv += 'c ' + str(c) + '\n'
+
+        return rv
+
+    def enc(self, samples):
+        '''encode the problem'''
+        # -x1 | -x2
+        self.add_constraint([neg(self.x(1)), neg(self.x(2))])
+        # x1 | x2
+        self.add_constraint([self.x(1), self.x(2)])
+        # x1 <=> x2
+        self.add_iff(self.x(3), self.x(4))
+        # y1 | (y2 & y3)
+        self.add_constraint([self.y(1), self.mk_and(self.y(2),self.y(3))])
+        # -y1
+        self.add_constraint([neg(self.y(1))])
+        
+def get_model(lns):
+    vals=dict()
+    found=False
+    for l in lns:
+        l=l.rstrip()
+        if not l: continue
+        if not l.startswith('v ') and not l.startswith('V '): continue
+        found=True
+        vs = l.split()[1:]
+        for v in vs:
+            if v == '0': break
+            vals[int(var(v))] = not sign(v)
+    return vals if found else None
+
+def parse(f):
+    nms = None
+    samples = []
+    for l in f:
+        s = l.rstrip().split()
+        if not s: continue
+        if nms:
+            samples.append([int(l) for l in s])
+        else:
+            nms = [int(l) for l in s]
+    return (nms, samples)
+
+
+
+if __name__ == "__main__":
+    debug_solver = False 
+
+    print("# reading from stdin")
+    nms, samples = parse(sys.stdin)
+    print("# encoding")
+    e = Enc(nms[0], nms[1])
+    e.enc(samples)
+    print("# encoded constraints")
+    print("# " + "\n# ".join(map(str, e.constraints)))
+    print("# END encoded constraints")
+    print("# sending to solver '" + solver + "'")
+    cnf = e.mk_cnf(False)
+    p = subprocess.Popen(solver, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (po, pe) = p.communicate(input=bytes(cnf, encoding ='utf-8'))
+    if debug_solver:
+        print('\n'.join(lns), file=sys.stderr)
+        print(cnf, file=sys.stderr)
+    print("# decoding result from solver")
+    rc = p.returncode
+    lns = str(po, encoding ='utf-8').splitlines()
+    if rc == 10:
+        e.print_model(get_model(lns))
+    elif rc == 20:
+        print("UNSAT")
+    else:
+        print("ERROR: something went wrong with the solver")
