61558f3e0d771938d53ac4c43d792e1d678bc2e0
Margarida <marghrid@hotmail.com>
2019-10-27 18:23:58 +0000

    Add option to time solver

--- Proj1/proj1.py
+++ Proj1/proj1.py
@@ -8,8 +8,7 @@ from enc import Enc, var, sign
 import argparse
 
 solver_dir = './solvers/'
-solvers = ['cadical', 'cryptominisat5_simple', 'MapleCBTCoreFirst', 'optsat', 'smallsat', 'lingeling', 'mergesat']
-solver_w_options = [['./solvers/cryptominisat5_simple', '--sls=walksat'], ['./solvers/cryptominisat5_simple', '--sls=yalsat']]
+solvers = ['cadical', 'cryptominisat5_simple', 'MapleCBTCoreFirst', 'optsat', 'smallsat', 'lingeling', 'mergesat', 'cryptominisat5_simple --sls=walksat', 'cryptominisat5_simple --sls=yalsat']
 solver = solver_dir + solvers[0]
 
 def get_model(lns):
@@ -48,8 +47,10 @@ if __name__ == "__main__":
 	argparser.add_argument('-m', '--print_model', action='store_true', help='print model')
 	argparser.add_argument('-c', '--print_constraints', action='store_true', help='print all encoded constraints')
 	argparser.add_argument('-v', '--verbose', action='store_true', help='print everything')
+	argparser.add_argument( '--time', action='store_true', help='time solver')
 	cmd_args = argparser.parse_args()
 
+	time = cmd_args.time
 	print_tree = cmd_args.print_tree
 	print_constraints = cmd_args.print_constraints
 	print_model = cmd_args.print_model
@@ -73,7 +74,9 @@ if __name__ == "__main__":
 
 	print("# sending to solver '" + str(solver) + "'")
 	cnf = e.mk_cnf(False)
-	p = subprocess.Popen(solver, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	if time:
+		solver = 'time -f "%E" ' + solver
+	p = subprocess.Popen(solver, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	(po, pe) = p.communicate(input=bytes(cnf, encoding ='utf-8'))
 	
 	if debug_solver:
@@ -83,6 +86,7 @@ if __name__ == "__main__":
 	print("# decoding result from solver")
 	rc = p.returncode
 	lns = str(po, encoding ='utf-8').splitlines()
+	lnse = str(pe, encoding ='utf-8').split()
 	
 	if rc == 10:
 		if print_model:
@@ -97,3 +101,6 @@ if __name__ == "__main__":
 		print("UNSAT")
 	else:
 		print("ERROR: something went wrong with the solver")
+
+	if time:
+		print('real (wall clock) time:', lnse[-1])
